cd769ffcefeb78b1404c03e8f38680a672945b50
Kem <tuetran25112002@gmail.com>
2023-09-11 09:06:42 +0700

    create temp and nav

---
+++ app/components/Avatar.tsx
@@ -0,0 +1,10 @@
+'use client'
+import Image from "next/image"
+
+const Avatar = () => {
+  return (
+    <Image src="/images/placeholder.jpg" className="rounded-full" height={30} width={30} alt="avatar" />
+  )
+}
+
+export default Avatar
